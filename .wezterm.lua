f67234e3619911c862dc51963cf54694430fd326
Yannik Peschke <yannikpeschke@pop-os.fritz.box>
2024-03-23 13:53:43 +0100

    remove not needed comments in wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.window_background_opacity = 0.85
+
+return config
