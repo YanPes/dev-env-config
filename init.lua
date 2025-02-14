11d3ff3405b71497537fef1354819f58e1a33ea5
yanpes <yannik.peschke@gmail.com>
2025-02-14 19:09:48 +0100

    add harpoon, colorscheme, fix go-to-definition binding, add go-back-to-prev binding

---
+++ init.lua
@@ -0,0 +1,119 @@
+--Global vim bindings
+vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git", lazypath
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+        -- Treesitter for syntax highlighting
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "typescript", "javascript", "tsx", "css", "lua" },
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
+    },
+
+    -- Auto-close jsx or tsx tags
+    {
+        "windwp/nvim-ts-autotag",
+        config = function()
+            require("nvim-ts-autotag").setup()
+        end,
+    },
+
+    -- LSP Config
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.ts_ls.setup({})
+            lspconfig.cssls.setup({})
+            lspconfig.html.setup({})
+            -- Go to Definition
+            vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true, silent = true })
+            -- Open Buffer Code Actions
+            vim.api.nvim_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', { noremap = true, silent = true })
+            -- Go back to previous Buffer (nice combination with "gd")
+            vim.api.nvim_set_keymap('n', 'gb', ':bprev<CR>', { noremap = true, silent = true })
+        end
+    },
+
+    -- File Explorer (NERDTree Alternative)
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("nvim-tree").setup()
+            vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+        end
+    },
+
+    -- Status Bar
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup()
+        end
+    },
+
+    -- Fuzzy Finder (Telescope)
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local telescope = require("telescope")
+            telescope.setup()
+            vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', '<leader>fs', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', { noremap = true, silent = true })
+        end
+    },
+
+    -- Harpoon
+    {
+        "ThePrimeagen/harpoon",
+        config = function()
+		vim.api.nvim_set_keymap('n', '<leader>ha', '<Cmd>lua require("harpoon.mark").add_file()<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<leader>hm', '<Cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', '<leader>1', '<Cmd>lua require("harpoon.ui").nav_file(1)<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', '<leader>2', '<Cmd>lua require("harpoon.ui").nav_file(2)<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', '<leader>3', '<Cmd>lua require("harpoon.ui").nav_file(3)<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', '<leader>4', '<Cmd>lua require("harpoon.ui").nav_file(4)<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', '<leader>5', '<Cmd>lua require("harpoon.ui").nav_file(5)<CR>', { noremap = true, silent = true })
+	end
+    }
+})
+
+-- Splits
+
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>sh", ":split<CR>", { noremap = true, silent = true })
+
+-- Terminals
+
+vim.keymap.set("n", "<leader>tv", ":vsplit | terminal<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>th", ":split | terminal<CR>", { noremap = true, silent = true })
+
+-- Color scheme
+
+vim.cmd("colorscheme habamax")
+
